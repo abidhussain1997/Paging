04f0553881e3e614f0cf3cb6adbb92e15cf77253
Abid Hussain <abidhussain3008@gmail.com>
2019-12-19 00:21:01 +0530

    add pushtoArray function

--- src/components/paging/index.js
+++ src/components/paging/index.js
@@ -15,7 +15,7 @@ export default class Paging extends React.Component {
     });
   };
 
-  updateTotalPage = e => {
+  setTotalPage = e => {
     const { currentPage } = this.state;
     let page = Math.floor(e.target.value);
     if (page < currentPage) {
@@ -34,28 +34,26 @@ export default class Paging extends React.Component {
     let { currentPage, totalPage } = this.state;
     let paging = [];
 
+    const pushToArray = (i) => {
+      paging.push(
+        i === currentPage ? (
+          <div key={i} className="current-page">
+            {i}
+          </div>
+        ) : (
+          <div key={i} onClick={() => this.setCurrentPage(i)}>{i}</div>
+        )
+      );
+    }
+
     if (currentPage <= 5) {
       if (totalPage < 10) {
         for (let i = 1; i <= totalPage; i++) {
-          paging.push(
-            i === currentPage ? (
-              <div key={i} className="current-page">
-                {i}
-              </div>
-            ) : (
-              <div key={i} onClick={() => this.setCurrentPage(i)}>{i}</div>
-            )
-          );
+          pushToArray(i)
         }
       } else {
         for (let i = 1; i <= 10; i++) {
-          paging.push(
-            i === currentPage ? (
-              <div key={i} className="current-page">{i}</div>
-            ) : (
-              <div key={i} onClick={() => this.setCurrentPage(i)}>{i}</div>
-            )
-          );
+          pushToArray(i)
         }
       }
     }
@@ -63,25 +61,13 @@ export default class Paging extends React.Component {
     if (currentPage > 5) {
       if (totalPage >= currentPage + 4) {
         for (let i = currentPage - 5; i <= currentPage + 4; i++) {
-          paging.push(
-            i === currentPage ? (
-              <div key={i} className="current-page">{i}</div>
-            ) : (
-              <div key={i} onClick={() => this.setCurrentPage(i)}>{i}</div>
-            )
-          );
+          pushToArray(i)
         }
       } else {
         let startIdx = currentPage - 9 + (totalPage - currentPage);
         if (startIdx < 1) startIdx = 1;
         for (let i = startIdx; i <= totalPage; i++) {
-          paging.push(
-            i === currentPage ? (
-              <div key={i} className="current-page">{i}</div>
-            ) : (
-              <div key={i} onClick={() => this.setCurrentPage(i)}>{i}</div>
-            )
-          );
+          pushToArray(i)
         }
       }
     }
@@ -96,7 +82,7 @@ export default class Paging extends React.Component {
           <div>
             Total Pages:{" "}
             <input
-              onChange={this.updateTotalPage}
+              onChange={this.setTotalPage}
               className="totalpage-input"
               type="number"
               min="1"
